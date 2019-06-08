17b0d3059c210935e75975db2fc27a6585dd8f84
Aaron Hayes <aaron.hayes92@gmail.com>
2019-06-08 11:29:30 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Apollo Express + Sofa + Swagger UI Example
+
+This is a basic example of settings up a GraphQL Server using Typescript and wrapping those endpoints with automatically generated REST APIs. We're using Sofa to use generate the REST APIs, and swagger to expose documentation.
+
+## Getting Started
+
+1. Install Node Depdencies `yarn`
+2. Start Express Server `yarn start`
+
+This produces three endpoints:
+
+- [GraphQL Server - http://localhost:4123/graphql](http://localhost:4123/graphql)
+- [Swagger UI - http://localhost:4123/docs](http://localhost:4123/docs)
+- [REST API Endpoing - http://localhost:4123/api](http://localhost:4123/api)
+
+## Further Readings
+
+- [Apollo Server](https://www.apollographql.com/docs/apollo-server/)
+- [Sofa REST](https://sofa-api.com)
+- [Swagger UI](https://github.com/scottie1984/swagger-ui-express)
+- [TypeScript](https://www.typescriptlang.org/)
---
+++ index.ts
@@ -0,0 +1,97 @@
+import { gql } from "apollo-server";
+import { ApolloServer, makeExecutableSchema } from "apollo-server-express";
+import express from "express";
+import sofa, { OpenAPI } from "sofa-api";
+import swaggerUi from "swagger-ui-express";
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+
+  type Mutation {
+    create_book(title: String!, author: String!): Book
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books
+  },
+  Mutation: {
+    create_book: (_: any, { title, author }: any) => {
+      books.push({ title, author });
+      return { title, author };
+    }
+  }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Swagger/OpenAPI Docs
+const openApi = OpenAPI({
+  schema,
+  info: {
+    title: "slidr Payment Gateway",
+    version: "1.0.0"
+  }
+});
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ schema });
+
+const app = express();
+server.applyMiddleware({ app, path: "/graphql" });
+
+// Sofa REST Endpoint
+app.use(
+  "/api",
+  sofa({
+    schema,
+    depthLimit: 3,
+    onRoute(info) {
+      openApi.addRoute(info, {
+        basePath: "/api"
+      });
+    }
+  })
+);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApi.get()));
+
+// Start App
+app.listen({ port: 4123 }, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:4123/graphql`);
+  console.log(`🚀 REST Server ready at http://localhost:4123/api`);
+  console.log(`🚀 Swagger Docs ready at http://localhost:4123/docs`);
+});
